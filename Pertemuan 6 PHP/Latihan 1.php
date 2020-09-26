92d3b4a47df16dde8eb4b97e9b59718e9d44af55
alihusain307 <m.alihusain357@gmail.com>
2020-09-26 12:01:08 +0800

    Pertemuan 6

---
+++ Pertemuan 6 PHP/Latihan 1.php
@@ -0,0 +1,15 @@
+<?php 
+$x = array('M. Ali Husain', '082251215097', 'alihusain.eltibiz07@gmail.com');
+
+    echo "Nama: ".$x[0];
+    echo "<br>WhatApp: ".$x[1];
+    echo "<br>Email: ".$x[2];
+
+
+$y = array('nama' => 'M. ALi Husain', 'wa' => '082251215097', 'email' => 'alihusain.eltibiz07@gmail.com');
+    echo "<br><br>";
+    echo "Nama: ".$y['nama'];
+    echo "<br>WhatApp: ".$y['wa'];
+    echo "<br>Email: ".$y['email'];
+
+ ?>
